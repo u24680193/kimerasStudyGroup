87badce8631e6416a6bba004716d4caa08b2ff84
kimer <kimer@KIMS_LAPTOP>
2025-03-03 21:55:56 +0200

    Add project files.

---
+++ kimerasStudyGroup/Controllers/MemberController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace kimerasStudyGroup.Controllers
+{
+    public class MemberController : Controller
+    {
+        // GET: Member
+        public ActionResult ListMember()
+        {
+            List<Models.StudyGroupMember> member = new List<Models.StudyGroupMember>();
+
+            member.Add(new Models.StudyGroupMember { StudentNumber = "u24680193", Name = "Kimera", Surname = "Perumal", EmailAddress = "u24680193@tuks.co.za" });
+            member.Add(new Models.StudyGroupMember { StudentNumber = "u24713407", Name = "Tiyana", Surname = "Vanmali", EmailAddress = "u24713407@tuks.co.za" });
+            member.Add(new Models.StudyGroupMember { StudentNumber = "u23646773", Name = "Arishna", Surname = "Rathod", EmailAddress = "u23646773@tuks.co.za" });
+            member.Add(new Models.StudyGroupMember { StudentNumber = "u23679485", Name = "Luyanda", Surname = "Mthethwa", EmailAddress = "23679485@tuks.co.za" });
+            member.Add(new Models.StudyGroupMember { StudentNumber = "u23539242", Name = "Mariam", Surname = "Abdul", EmailAddress = "u23539242@tuks.co.za" });
+
+            return View(member);
+        }
+    }
+}
---
+++ kimerasStudyGroup/Models/StudyGroupMember.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace kimerasStudyGroup.Models
+{
+    public class StudyGroupMember
+    {
+        [Display(Name = "STUDENT NUMBER")] // Add as decorator
+        public string StudentNumber { get; set; }
+
+        [Display(Name = "NAME")] // Add as decorator
+        public string Name { get; set; }
+
+        [Display(Name = "SURNAME")] // Add as decorator
+        public string Surname { get; set; }
+
+        [Display(Name = "EMAIL ADDRESS")] // Add as decorator
+        public string EmailAddress { get; set; }
+    }
+}
